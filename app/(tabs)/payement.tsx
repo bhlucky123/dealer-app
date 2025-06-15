f9a8bcac0062ab3e27e6bee8f370370aedefa1f1
bhlucky123 <luckybh2025@gmail.com>
2025-06-15 14:51:31 +0530

    dir change

---
+++ app/(tabs)/payement.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function PaymentTab() {
+    return (
+        <View className='flex items-center justify-center h-full'>
+            <Text className='text-lg font-bold text-white '>Payment Page</Text>
+        </View>
+    )
+}
