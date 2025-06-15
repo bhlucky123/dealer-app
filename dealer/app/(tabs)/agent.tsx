1d8a59dabd50bded2bb5c509b414f71c6dd4350d
bhlucky123 <luckybh2025@gmail.com>
2025-06-15 14:40:54 +0530

    login page

---
+++ dealer/app/(tabs)/agent.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function AgentTab() {
+  return (
+   <View className='flex items-center justify-center h-full'>
+    <Text className='text-lg font-bold text-white '>Agent Page</Text>
+   </View>
+  );
+}
