5d5dab768bfc7f2c284f1dfc6190a17f988391a6
bhlucky123 <luckybh2025@gmail.com>
2025-06-15 14:40:54 +0530

    login page

---
+++ dealer/app/(tabs)/more.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function MoreTab() {
+    return (
+        <View className='flex items-center justify-center h-full'>
+            <Text className='text-lg font-bold text-white '>More Page</Text>
+        </View>
+    )
+}
